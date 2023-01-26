cc4af3e1f85bef02a8ff4bcbf7198576b3482110
Davran_Djumanov <dda_82@mail.ru>
2023-01-26 22:36:27 +0600

    add Example 005

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write(" ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Паша");
+{
+    Console.WriteLine("Ура это же Паша !"); 
+} 
+{
+    
+}
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
